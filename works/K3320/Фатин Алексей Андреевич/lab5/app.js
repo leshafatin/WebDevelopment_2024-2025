8510b0705b21cdd1b27386ae723dd09a5f6e463b
leshafatin <70691537+leshafatin@users.noreply.github.com>
2025-03-30 17:20:10 +0300

    fix lab1

--- works/K3320/Фатин Алексей Андреевич/lab5/app.js
+++ works/K3320/Фатин Алексей Андреевич/lab5/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const port = 888;
+const port = 12312;
 
 app.use(express.static(path.join(__dirname)));
 
